5b40f36d8e0baf43253005cc2f1f7df6a2bd51d1
patra <patra@DESKTOP-NG8K2H1>
2023-07-07 18:29:53 +0530

    Added env

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,13 @@
 import mongoose, { mongo } from "mongoose";
+import dotenv from 'dotenv';
 
-const DB_URL = `mongodb+srv://mrx010:EVfk7tG4fwukQdL7@cluster0.sdugpml.mongodb.net/?retryWrites=true&w=majority`;
+dotenv.config();
+
+
 const dbCon = async () => {
+    // const DB_URL = process.env.MONGO_URI;
     try {
-        await mongoose.connect(DB_URL, {useNewUrlParser: true});
+        await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
         console.log('Database connected successfully!');
     } catch (error) {
         console.error('Error while connecting the DB ', error.message);
--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import router from './Routes/routes.js';
 import cors from 'cors';
 import dbCon from './db/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use('/', router);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 dbCon();
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
