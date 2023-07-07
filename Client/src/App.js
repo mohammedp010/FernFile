f52043d24833957dea6755e64d0d4f0de6a9155b
patra <patra@DESKTOP-NG8K2H1>
2023-07-07 11:45:59 +0530

    Initial setup and frontend

---
+++ Client/src/App.js
@@ -0,0 +1,43 @@
+import {useRef,useState,useEffect} from 'react';
+import './App.css';
+import { uploadFile } from './services/api';
+
+
+function App() {
+  const fileInputRef = useRef();
+
+  const [file, setFile] = useState('');
+
+const onUploadClick = () => {
+  fileInputRef.current.click();
+}
+  useEffect(() => {
+    const getImage = async () =>{
+      if(file)
+      {
+        const data = new FormData();
+        data.append("name", file.name)
+        data.append("file", file)
+
+        let response = await uploadFile(data);
+      }
+    }
+    getImage();
+  }, [file])
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h1>FERN</h1>
+        <h3>Simple file sharing application!</h3>
+        <button onClick= {() => onUploadClick()}>Upload</button>
+        <input
+        type='file' ref={fileInputRef} 
+        style={{display: 'none'}}
+        onChange={(e) => setFile(e.target.files[0])}
+         />
+      </div>
+    </div>
+  );
+}
+
+export default App;
